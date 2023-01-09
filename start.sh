4380ea046ae504e0a27282c23d8904bc5851fbdc
BrokenBoy204 <111448245+Brokenboy204@users.noreply.github.com>
2023-01-09 16:18:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-V5-Without-UrlShortner
 fi
-cd /Auto-Filter-V5
+cd /Auto-Filter-V5-Without-UrlShortner
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
