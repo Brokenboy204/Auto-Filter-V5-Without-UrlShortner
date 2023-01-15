72e71d827459f6dca3157c2388c735f4628adc1b
BrokenBoy204 <111448245+Brokenboy204@users.noreply.github.com>
2023-01-15 16:07:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 FILDLT_CNL = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('FILDLT_CNL', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<Asmovies204>:<Anshuu12>@cluster0.izecre3.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<anshseth>:<Anshuu12>@cluster0.izecre3.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "EvaMaria")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
